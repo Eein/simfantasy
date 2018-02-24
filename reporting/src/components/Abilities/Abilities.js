428b85aacf4e5a3c76eb6b822a7a79e191f2ebc2
William Volin <me@williamvolin.com>
2018-02-23 19:25:38 -0700

    start on abilities report

---
+++ reporting/src/components/Abilities/Abilities.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+export default class Abilities extends Component {
+
+  totalDamage() {
+    const { data } = this.props
+    let damage = 0.0
+    for(let action in data.actions) {
+      for(let d in data.actions[action].damage) {
+        damage += data.actions[action].damage[d].damage
+      }
+    }
+    return damage
+  }
+
+  render() {
+    const { data } = this.props
+    const damage = this.totalDamage()
+    const dps = damage / data.combatLength
+    return (
+      <div>
+        <Field grouped>
+          <Control>
+          <Tag dark>Name: {data.name}</Tag>
+        </Control>
+          <Control>
+          <Tag dark>Race: {data.race}</Tag>
+        </Control>
+          <Control>
+          <Tag dark>DPS: {Math.round(dps)}</Tag>
+        </Control>
+          <Control>
+          <Tag dark>Damage: {damage}</Tag>
+        </Control>
+        </Field>
+      </div>
+    )
+  }
+}
